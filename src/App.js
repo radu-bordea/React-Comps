8ffe2f9be1cb56d91a169fb715e546593e709072
radu-bordea <radu.bordea.dev@gmail.com>
2024-03-19 11:25:34 +0100

    create ModalPage and ModalComponent and add path to the side bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import Sidebar from './components/Sidebar'; // Importing the Sidebar component
-import Route from './components/Route'; // Importing the Route component
-import AccordionPage from './pages/AccordionPage'; // Importing the AccordionPage component
-import DropdownPage from './pages/DropdownPage'; // Importing the DropdownPage component
-import ButtonPage from './pages/ButtonPage'; // Importing the ButtonPage component
+import Sidebar from "./components/Sidebar"; // Importing the Sidebar component
+import Route from "./components/Route"; // Importing the Route component
+import AccordionPage from "./pages/AccordionPage"; // Importing the AccordionPage component
+import DropdownPage from "./pages/DropdownPage"; // Importing the DropdownPage component
+import ButtonPage from "./pages/ButtonPage"; // Importing the ButtonPage component
+import ModalPage from "./pages/ModalPage";
 
 function App() {
   return (
-    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4"> {/* Main container */}
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      {" "}
+      {/* Main container */}
       <Sidebar /> {/* Sidebar component */}
-      <div className="col-span-5"> {/* Main content area, spanning 5 columns */}
-        <Route path="/accordion"> {/* Route for the AccordionPage */}
+      <div className="col-span-5">
+        {" "}
+        {/* Main content area, spanning 5 columns */}
+        <Route path="/accordion">
+          {" "}
+          {/* Route for the AccordionPage */}
           <AccordionPage /> {/* AccordionPage component */}
         </Route>
-        <Route path="/"> {/* Default route for the DropdownPage */}
+        <Route path="/">
+          {" "}
+          {/* Default route for the DropdownPage */}
           <DropdownPage /> {/* DropdownPage component */}
         </Route>
-        <Route path="/buttons"> {/* Route for the ButtonPage */}
+        <Route path="/buttons">
+          {" "}
+          {/* Route for the ButtonPage */}
           <ButtonPage /> {/* ButtonPage component */}
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
         </Route>
       </div>
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
-import Link from './Link'; // Importing Link component
+import Link from "./Link"; // Importing Link component
 
 function Sidebar() {
   // Array of sidebar links with labels and paths
   const links = [
-    { label: 'Dropdown', path: '/' },
-    { label: 'Accordion', path: '/accordion' },
-    { label: 'Buttons', path: '/buttons' },
+    { label: "Dropdown", path: "/" },
+    { label: "Accordion", path: "/accordion" },
+    { label: "Buttons", path: "/buttons" },
+    { label: "Modal", path: "/modal" },
   ];
 
   // Mapping through links to render Link components
